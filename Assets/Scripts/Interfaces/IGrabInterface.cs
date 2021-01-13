d70acd4b3f784d5997d5925b827b32dc0f0ae74c
Pjotr Svetachov <7099961+PjotrSvetachov@users.noreply.github.com>
2021-01-13 15:49:24 +0100

    Added missing file from last commit.

---
+++ Assets/Scripts/Interfaces/IGrabInterface.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGrabInterface
+{
+    void ControllerEnter();
+    void ControllerExit();
+    void ControllerGrabBegin(GameObject parent);
+    void ControllerGrabEnd();
+}
