f70e18757e220dbc9bb8873f757c2cc953d283a5
Max Postema <46395349+MaxPostema@users.noreply.github.com>
2021-02-10 14:06:49 +0100

    feat: Add VR text entry to UI InputFields
    On adding a AutoOpenKeyboard MonoBehaviour to a GameObject with a InputField. The inputfield will open a VR Keys keyboard for text entry

---
+++ Assets/Scripts/UI/AutoOpenKeyboard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/*
+ * Add this to GameObject with a InputField to automagicly open a VR keyboard to input text
+ * Need a VRKeys GameObject with a KeyboardHandler MonoBehaviour
+ */
+
+public class AutoOpenKeyboard : MonoBehaviour, ISelectHandler
+{
+    private InputField input;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        KeyboardHandler.instance.Open(input);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        KeyboardHandler.instance.Close();
+    }
+
+    void Start()
+    {
+        input = GetComponent<InputField>();
+    }
+}
---
+++ Assets/Scripts/UI/KeyboardHandler.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRKeys;
+
+/*
+ * Helper Singleton to open and close the VR keys module
+ * Use:
+ * KeyboardHandler.instance.Open(InputField) to open a keyboard 
+ * KeyboardHandler.instance.Close() to close the vr keyboard
+ */
+
+public class KeyboardHandler : MonoBehaviour
+{
+    public Camera cameraToFollow;
+    public Vector3 offset;
+    private Keyboard keyboard;
+    private InputField inputField;
+    private string originalStringValue; // Use to restore string on cancel
+    static public KeyboardHandler instance;
+    
+
+    public void Open(InputField input)
+    {
+        keyboard.Enable();
+        keyboard.SetPlaceholderMessage("Please enter something");
+
+        keyboard.OnUpdate.AddListener(HandleUpdate);
+        keyboard.OnSubmit.AddListener(HandleSubmit);
+        keyboard.OnCancel.AddListener(HandleCancel);
+        inputField = input;
+        originalStringValue = input.text;
+        UpdateLocation();
+    }
+
+    public void Close()
+    {
+        keyboard.OnUpdate.RemoveListener(HandleUpdate);
+        keyboard.OnSubmit.RemoveListener(HandleSubmit);
+        keyboard.OnCancel.RemoveListener(HandleCancel);
+
+        keyboard.Disable();
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        keyboard = GetComponent<Keyboard>();
+    }
+
+    private void UpdateLocation()
+    {
+        Quaternion rotation = Quaternion.Euler(0, cameraToFollow.transform.rotation.eulerAngles.y, 0);
+        transform.rotation = rotation;
+        transform.position = cameraToFollow.transform.position + (rotation * offset);
+    }
+
+    public void HandleUpdate(string text)
+    {
+        keyboard.HideValidationMessage();
+        inputField.text = text;
+    }
+
+    public void HandleSubmit(string text)
+    {
+        keyboard.DisableInput();
+        Close();
+    }
+
+    public void HandleCancel()
+    {
+        // Restore the string to what it was when opening the VR keyboard
+        inputField.text = originalStringValue; 
+        keyboard.SetText(originalStringValue);
+        Close();
+    }
+}
