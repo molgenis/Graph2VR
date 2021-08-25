68c47fd183cd1e3aaca49e0b171103b19828dfe9
Pjotr Svetachov <7099961+PjotrSvetachov@users.noreply.github.com>
2021-08-25 15:36:20 +0200

    Reworked rotation a bit, now you can tilt the sphere by tilting the controllers, still not perfect but getting there.

--- Assets/Scripts/UI/SphereRotation/SphereInteraction.cs
+++ Assets/Scripts/UI/SphereRotation/SphereInteraction.cs
@@ -12,18 +12,24 @@ public class SphereInteraction : MonoBehaviour
     Transform leftControler;
     Transform rightControler;
 
-    Vector3 lefToRight; // In worldspace
+    // In worldspace
     Vector3 leftToCenter;
     float handleDistance;
     Vector3 initialScale;
     Quaternion initialRotation;
+
+    Quaternion initialLookat;
+
 
     void StartInteraction()
     {
         Vector3 center = bsphere.transform.position;
         Vector3 left = leftControler.transform.position;
         Vector3 right = rightControler.transform.position;
-        lefToRight = right - left;
+
+        Vector3 initialForward = (leftControler.transform.forward + rightControler.transform.forward) * 0.5f;
+        initialLookat = Quaternion.LookRotation(initialForward, (right - left).normalized);
+
         leftToCenter = center - left;
         handleDistance = Vector3.Distance(left, right);
         initialScale = transform.localScale;
@@ -40,10 +46,12 @@ public class SphereInteraction : MonoBehaviour
         Vector3 left = leftControler.transform.position;
         Vector3 right = rightControler.transform.position;
 
-        Vector3 newLefToRight = right - left;
+        Vector3 forward = (leftControler.transform.forward + rightControler.transform.forward) * 0.5f;
+        Quaternion lookat = Quaternion.LookRotation(forward, (right - left).normalized);
+
         float sizeFactor = Vector3.Distance(left, right) / handleDistance;
 
-        Quaternion rotation = Quaternion.FromToRotation(lefToRight, newLefToRight);
+        Quaternion rotation = lookat * Quaternion.Inverse(initialLookat);
         Vector3 center = (left + ((rotation * leftToCenter)*sizeFactor));
 
         transform.position = center + (transform.position - bsphere.transform.position);
