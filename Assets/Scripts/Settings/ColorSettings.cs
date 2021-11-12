e3fcc08ad8107b6253382f4f63cdf9bb5814bc10
Alexander Kellmann <a.j.kellmann@umcg.nl>
2021-11-12 15:07:39 +0100

    Adding / Changing Colors
    Taking the Colors from VOWL and adding them

--- Assets/Scripts/Settings/ColorSettings.cs
+++ Assets/Scripts/Settings/ColorSettings.cs
@@ -10,14 +10,44 @@ public class ColorSettings : MonoBehaviour
     instance = this;
   }
 
-  public Color defaultNodeColor;
-  public Color defaultEdgeColor;
+//Edges:
+//
+//Foreground 	#000 	black 	lines, borders, arrowheads, text, numbers
+    public Color defaultEdgeColor;
+// Indirect Highlighting 	#f90 	orange 	rectangles, circles
   public Color edgeSelectedColor;
+// Hover  #fff   white lines
   public Color edgeHoverColor;
+// Highlighting 	#f00 	red lines, arrowheads
   public Color edgeGrabbedColor;
 
+
+//Nodes
+  public Color defaultNodeColor;
   public Color uriColor;
+// Datatype 	#fc3 	yellow 	rdfs:Datatype, rdfs:Literal
   public Color literalColor;
   public Color variableColor;
   public Color blankNodeColor;
+// Indirect Highlighting 	#f90 	orange 	nodes
+  public Color nodeSelectedColor;
+// Hover  #fff   white nodes
+  public Color nodeHoverColor;
+//Highlighting 	#f00 	red nodes
+  public Color nodeGrabbedColor;
+
+
+
+// VOWL color schema
+//ToDo: Implement those
+// General 	#acf 	light blue 	owl:Class, owl:ObjectProperty (incl. subclasses)
+  public Color nodeOwlClassColor;
+// Rdf 	#c9c 	light purple 	rdfs:Class, rdfs:Resource, rdf:Property
+  public Color nodeRdfsClassColor;
+// Datatype Property 	#9c6 	light green owl:DatatypeProperty
+  public Color nodeOwlDatatypeColor;
+// Deprecated 	#ccc 	light gray 	owl:DeprecatedClass, owl:DeprecatedProperty
+  public Color DeprecatedColor;
+    // Neutral 	#fff 	white   owl:Thing, arrowhead of rdfs:subClassOf
+  public Color arrowheadSubclassOfColor;
 }
