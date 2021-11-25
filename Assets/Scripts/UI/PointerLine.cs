937c8b5431d426f369039ba84230643324244b60
Max Postema <46395349+MaxPostema@users.noreply.github.com>
2021-11-25 09:35:52 +0100

    feat: use urp

---
+++ Assets/Scripts/UI/PointerLine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerLine : MonoBehaviour
+{
+  public int maxDistance = 10;
+  public LineRenderer line;
+  void Start()
+  {
+  }
+
+  void Update()
+  {
+    RaycastHit hit;
+    Vector3 endpoint = transform.position + (transform.forward * maxDistance);
+    if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance)) {
+      endpoint = hit.point;
+    }
+    line.SetPosition(0, transform.position);
+    line.SetPosition(1, endpoint);
+  }
+}
