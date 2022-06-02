1dca38cb886ff76480b81e2ee8500286e28d6114
Max Postema <46395349+MaxPostema@users.noreply.github.com>
2022-06-02 17:00:13 +0200

    fix: quest pointer direction

---
+++ Assets/Scripts/Interfaces/PointerCorrections.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerCorrections : MonoBehaviour
+{
+  public Vector3 questOffset = Vector3.zero;
+  public Vector3 questRotation = Vector3.zero;
+
+  public Vector3 viveOffset = Vector3.zero;
+  public Vector3 viveRotation = Vector3.zero;
+
+  void Start()
+  {
+    ControllerType.instance.GetControllerName((string name) =>
+    {
+      if (name == "quest")
+      {
+        transform.localPosition = questOffset;
+        transform.rotation = Quaternion.Euler(questRotation);
+      }
+      else
+      {
+        transform.localPosition = viveOffset;
+        transform.rotation = Quaternion.Euler(viveRotation);
+      }
+    });
+  }
+}
