abf26093618e6a86d9e040630701b16c5e9f849f
Max Postema <46395349+MaxPostema@users.noreply.github.com>
2022-06-28 11:11:28 +0200

    fix: dont select edge and node together

--- Assets/Scripts/Utils/EdgeInteraction.cs
+++ Assets/Scripts/Utils/EdgeInteraction.cs
@@ -3,63 +3,63 @@ using UnityEngine.EventSystems;
 
 public class EdgeInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IGrabInterface
 {
-  private Edge edge;
-  private Graph graph;
+   private Edge edge;
+   private Graph graph;
 
-  public void Start()
-  {
-    edge = GetComponent<Edge>();
-    graph = transform.parent.GetComponent<Graph>();
-  }
+   public void Start()
+   {
+      edge = GetComponent<Edge>();
+      graph = transform.parent.GetComponent<Graph>();
+   }
 
-  public void OnPointerClick(PointerEventData eventData)
-  {
-    Edge lastSelection = null;
-    if (graph == null) return;
+   public void OnPointerClick(PointerEventData eventData)
+   {
+      Edge lastSelection = null;
+      if (graph == null) return;
 
-    foreach (Edge graphEdge in graph.edgeList)
-    {
-      if (graphEdge.IsActiveInMenu) lastSelection = graphEdge;
-      graphEdge.IsActiveInMenu = false;
-    }
-    Edge edge = GetComponent<Edge>();
-    edge.IsActiveInMenu = true;
+      foreach (Edge graphEdge in graph.edgeList)
+      {
+         if (graphEdge.IsActiveInMenu) lastSelection = graphEdge;
+         graphEdge.IsActiveInMenu = false;
+      }
+      Edge edge = GetComponent<Edge>();
+      edge.IsActiveInMenu = true;
 
-    if (edge != lastSelection)
-    {
-      GameObject.FindGameObjectWithTag("LeftController").BroadcastMessage("Clear", SendMessageOptions.DontRequireReceiver);
-      GameObject.FindGameObjectWithTag("LeftController").BroadcastMessage("PopulateEdge", edge, SendMessageOptions.DontRequireReceiver);
-    }
-  }
+      if (edge != lastSelection)
+      {
+         GameObject.FindGameObjectWithTag("LeftController").BroadcastMessage("Close", SendMessageOptions.DontRequireReceiver);
+         GameObject.FindGameObjectWithTag("LeftController").BroadcastMessage("PopulateEdge", edge, SendMessageOptions.DontRequireReceiver);
+      }
+   }
 
 
-  void IGrabInterface.ControllerEnter()
-  {
-    edge.IsControllerHovered = true;
-  }
+   void IGrabInterface.ControllerEnter()
+   {
+      edge.IsControllerHovered = true;
+   }
 
-  void IGrabInterface.ControllerExit()
-  {
-    edge.IsControllerHovered = false;
-  }
+   void IGrabInterface.ControllerExit()
+   {
+      edge.IsControllerHovered = false;
+   }
 
-  void IGrabInterface.ControllerGrabBegin(GameObject newParent)
-  {
-    edge.IsControllerGrabbed = true;
-  }
+   void IGrabInterface.ControllerGrabBegin(GameObject newParent)
+   {
+      edge.IsControllerGrabbed = true;
+   }
 
-  void IGrabInterface.ControllerGrabEnd()
-  {
-    edge.IsControllerGrabbed = false;
-  }
+   void IGrabInterface.ControllerGrabEnd()
+   {
+      edge.IsControllerGrabbed = false;
+   }
 
-  void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
-  {
-    edge.IsPointerHovered = true;
-  }
+   void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+   {
+      edge.IsPointerHovered = true;
+   }
 
-  void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
-  {
-    edge.IsPointerHovered = false;
-  }
+   void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+   {
+      edge.IsPointerHovered = false;
+   }
 }
--- Assets/Scripts/Utils/NodeInteraction.cs
+++ Assets/Scripts/Utils/NodeInteraction.cs
@@ -3,68 +3,68 @@ using UnityEngine.EventSystems;
 
 public class NodeInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IGrabInterface
 {
-  private Transform originalParent;
-  private Graph graph;
-  private Node node;
+   private Transform originalParent;
+   private Graph graph;
+   private Node node;
 
-  public void Start()
-  {
-    node = GetComponent<Node>();
-    originalParent = transform.parent;
-    graph = originalParent.GetComponent<Graph>();
-  }
+   public void Start()
+   {
+      node = GetComponent<Node>();
+      originalParent = transform.parent;
+      graph = originalParent.GetComponent<Graph>();
+   }
 
-  public void OnPointerClick(PointerEventData eventData)
-  {
-    Node lastSelection = null;
-    foreach (Node graphNode in graph.nodeList)
-    {
-      if (graphNode.IsActiveInMenu) lastSelection = graphNode;
-      graphNode.IsActiveInMenu = false;
-    }
-    Node node = GetComponent<Node>();
-    node.IsActiveInMenu = true;
-    if (node != lastSelection)
-    {
-      GameObject.FindGameObjectWithTag("LeftController").BroadcastMessage("Clear", SendMessageOptions.DontRequireReceiver);
-      GameObject.FindGameObjectWithTag("LeftController").BroadcastMessage("PopulateNode", node, SendMessageOptions.DontRequireReceiver);
-    }
-  }
+   public void OnPointerClick(PointerEventData eventData)
+   {
+      Node lastSelection = null;
+      foreach (Node graphNode in graph.nodeList)
+      {
+         if (graphNode.IsActiveInMenu) lastSelection = graphNode;
+         graphNode.IsActiveInMenu = false;
+      }
+      Node node = GetComponent<Node>();
+      node.IsActiveInMenu = true;
+      if (node != lastSelection)
+      {
+         GameObject.FindGameObjectWithTag("LeftController").BroadcastMessage("Close", SendMessageOptions.DontRequireReceiver);
+         GameObject.FindGameObjectWithTag("LeftController").BroadcastMessage("PopulateNode", node, SendMessageOptions.DontRequireReceiver);
+      }
+   }
 
-  void IGrabInterface.ControllerEnter()
-  {
-    node.IsControllerHovered = true;
-  }
+   void IGrabInterface.ControllerEnter()
+   {
+      node.IsControllerHovered = true;
+   }
 
-  void IGrabInterface.ControllerExit()
-  {
-    node.IsControllerHovered = false;
-  }
+   void IGrabInterface.ControllerExit()
+   {
+      node.IsControllerHovered = false;
+   }
 
-  void IGrabInterface.ControllerGrabBegin(GameObject newParent)
-  {
-    node.IsControllerGrabbed = true;
+   void IGrabInterface.ControllerGrabBegin(GameObject newParent)
+   {
+      node.IsControllerGrabbed = true;
 
-    this.transform.SetParent(newParent.transform, true);
-    graph.layout.Stop();
-  }
+      this.transform.SetParent(newParent.transform, true);
+      graph.layout.Stop();
+   }
 
-  void IGrabInterface.ControllerGrabEnd()
-  {
-    node.IsControllerGrabbed = false;
+   void IGrabInterface.ControllerGrabEnd()
+   {
+      node.IsControllerGrabbed = false;
 
-    this.transform.SetParent(originalParent, true);
-    // NOTE: make some button to trigger the solver again. We dont always want it to solve. Sometimes we want the move the nodes ourself
-    // graph.layout.CalculateLayout(); 
-  }
+      this.transform.SetParent(originalParent, true);
+      // NOTE: make some button to trigger the solver again. We dont always want it to solve. Sometimes we want the move the nodes ourself
+      // graph.layout.CalculateLayout(); 
+   }
 
-  void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
-  {
-    node.IsPointerHovered = true;
-  }
+   void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+   {
+      node.IsPointerHovered = true;
+   }
 
-  void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
-  {
-    node.IsPointerHovered = false;
-  }
+   void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+   {
+      node.IsPointerHovered = false;
+   }
 }
