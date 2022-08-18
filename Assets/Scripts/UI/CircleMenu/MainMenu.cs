2d082717f53748872f799f277a548da5755b80b3
Max Postema <46395349+MaxPostema@users.noreply.github.com>
2022-08-18 10:47:49 +0200

    fix: rotate to camera, fix: make the checkbox readable

--- Assets/Scripts/UI/CircleMenu/MainMenu.cs
+++ Assets/Scripts/UI/CircleMenu/MainMenu.cs
@@ -166,7 +166,10 @@ public class MainMenu : BaseMenu
     cm.AddButton(Icon("\uF05A") + "Show help", Color.blue, () =>
     {
       Transform helpMenu = GameObject.FindGameObjectWithTag("HelpMenu").transform;
-      helpMenu.position = Camera.main.transform.position + (Camera.main.transform.forward * 2);
+      helpMenu.position = Camera.main.transform.position + (Camera.main.transform.forward * 1.5f);
+      Vector3 lookDirection = (helpMenu.position - Camera.main.transform.position).normalized;
+      helpMenu.rotation = Quaternion.LookRotation(lookDirection);
+
       helpMenu.Find("Help Menu Container").gameObject.SetActive(true);
     });
   }
