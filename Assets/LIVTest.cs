500405794eeb5fca53c2a4acabf319fd86817937
Max Postema <46395349+MaxPostema@users.noreply.github.com>
2023-09-08 11:30:12 +0200

    Added LIV compatibility, added a animated logo scene for use in the tutorials

---
+++ Assets/LIVTest.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LIVTest : MonoBehaviour
+{
+    void Update()
+    {  
+      //GameObject.FindGameObjectWithTag("LIV").GetComponent<LIV.SDK.Unity.LIV>().render.SetPose(new Vector3(0,-10,0), Quaternion.identity, 60, false);
+    }
+}
